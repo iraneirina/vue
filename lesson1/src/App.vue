962a99a14a47f83f1b75760909488e8a67218d2c
lebedevairina <malakhova.iv@outlook.com>
2022-06-09 14:59:38 +0300

    hw1

---
+++ lesson1/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="msg"/>
+    <p>{{ msg }}</p>
+    <label for="someText">
+      <input
+        id="someText"
+        type="text"
+        placeholder="text"
+        v-model="someText"
+        @focus="focus"
+        @blur="blur"
+      >
+    </label>
+    <p>{{ someText }}</p>
+    <CalcComponent />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CalcComponent from './components/Calc.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'Hello Vue!',
+      counter: 0,
+      someText: ''
+    }
+  },
+  methods: {
+    inc (step, event) {
+      console.log(event)
+      // eslint-disable-next-line no-plusplus
+      this.counter++
+    },
+    reset () {
+      this.counter = 0
+    },
+    focus () {
+      console.log('focus')
+    },
+    blur () {
+      console.log('blur')
+    }
+  },
+  components: {
+    HelloWorld,
+    CalcComponent
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
